65cc2da6f95a05add0628f28c640c189908bacd5
Marek Aufart <aufi.cz@gmail.com>
2019-03-30 14:12:41 +0100

    listen to input dev only

--- lib/evok-to-mqtt/mappers/to_haab.rb
+++ lib/evok-to-mqtt/mappers/to_haab.rb
@@ -16,28 +16,34 @@ module EvokToMqtt
         # Neuron input is assumed for now
         if @statuses[id]
           if @statuses[id][:value] == 0 && evok_event['value'] == 1
-            mqtt.publish id, {action: 'down', data: payload}
+            #mqtt.publish id, {action: 'down', data: payload}
           elsif @statuses[id][:value] == 1 && evok_event['value'] == 0
-            mqtt.publish id, {action: 'up', data: payload}
-            mqtt.publish id, {action: 'click', data: payload} if now - @statuses[id][:changed_at] < 2  # seconds
-            # ignore events not changing value
+            #mqtt.publish id, {action: 'up', data: payload}
+            if now - @statuses[id][:changed_at] < 2  # seconds
+              mqtt.publish id, {action: 'click', data: payload}
+              puts "### click"
+            else
+              mqtt.publish id, {action: 'long_click', data: payload}
+              puts "### long_click"
+            end
           end
           @statuses[id][:value] = evok_event['value']
           @statuses[id][:changed_at] = now
         else
           @statuses[id] = {value: evok_event['value'], changed_at: now}
-          mqtt.publish id, {action: 'down', data: payload}
+          #mqtt.publish id, {action: 'down', data: payload} unless evok_event['value'] == 0  # ignore initial states received in batch
         end
+        puts
       end
 
       private
 
       def get_topic(dev, circuit)
         begin
-          return @mapping[dev][circuit] || "evok_raw/#{dev}/#{circuit}" # missing circuit
+          return @mapping[dev][circuit] || "evok/#{dev}/#{circuit}" # missing circuit
         rescue => ex
           puts "Warning: #{ex}, using raw topic"
-          return "evok_raw/#{dev}/#{circuit}" # missing section (dev)
+          return "evok/#{dev}/#{circuit}" # missing section (dev)
         end
       end
     end
--- lib/evok-to-mqtt/worker.rb
+++ lib/evok-to-mqtt/worker.rb
@@ -16,16 +16,19 @@ module EvokToMqtt
       EM.run do
         @evok = WebSocket::EventMachine::Client.connect(:uri => "ws://#{@evok_host}:8080/ws")
         @mqtt = EventMachine::MQTT::ClientConnection.connect(@mqtt_host)
+        #@evok_rpc = JSONRPC::Client.new(evok_host)
 
         @evok.onmessage do |msg|
-          JSON.parse(msg).each do |event|
-            next if event['dev'] == 'wd'  # skip watchdog events
+          data = JSON.parse(msg)
+          data = [data] if data.is_a? Hash # temp is not in array in evok messages, but in hash..
+          data.each do |event|
+            next if !event.is_a?(Hash) || %w(wd ai ao relay).include?(event['dev']) # want just input and temp, skip the rest for now
             puts "Recieved message: #{event}"
             @mapper.process(@mqtt, event)
           end
         end
 
-        @mqtt.subscribe('neuron/output/relay/#')
+        @mqtt.subscribe('neuron/relay/#')
         @mqtt.receive_callback do |msg|
           # WS set did not work, using REST for now
           data = JSON.parse(msg.payload)
